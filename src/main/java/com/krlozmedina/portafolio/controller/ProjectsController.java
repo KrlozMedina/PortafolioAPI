acae0e64bd46f412abe677d36a673684e36c3f87
camedina <camedinal@correo.udistrital.edu.co>
2023-06-06 19:08:56 -0500

    [CORS] Agregando cors

--- src/main/java/com/krlozmedina/portafolio/controller/ProjectsController.java
+++ src/main/java/com/krlozmedina/portafolio/controller/ProjectsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class ProjectsController {
     ProjectRepository projectRepository;
 
     @GetMapping
+    @CrossOrigin(origins = "*")
     public ResponseEntity<Page<ListProjectDTO>> listProjects(Pageable pageable) {
         return ResponseEntity.ok(projectRepository.findAll(pageable).map(ListProjectDTO::new));
     }
